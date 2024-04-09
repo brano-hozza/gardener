4fd2f79fb9c56306c026316623dcd26bda13edba
Branislav Hozza <brankohozza@gmail.com>
2024-04-09 20:48:16 +0200

    :bug: higher z-index

--- src/components/gardener-error/gardener-error.tsx
+++ src/components/gardener-error/gardener-error.tsx
@@ -9,7 +9,7 @@ export class GardenerError {
 
   render() {
     return (
-      <div class="absolute w-full h-full bg-black bg-opacity-50">
+      <div class="absolute w-full h-full bg-black bg-opacity-50 z-50">
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md">
           <h1 class="text-2xl text-red-500 font-bold">Error</h1>
           <p>{this.error}</p>
